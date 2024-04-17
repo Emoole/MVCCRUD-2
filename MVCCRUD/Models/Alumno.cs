3cdfb59fb004afc72cbf53cbe033ddf2d1865b52
emoole <emonlop@outlook.com>
2024-04-17 10:10:03 -0500

    Agregar archivos de proyecto.

---
+++ MVCCRUD/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models;
+
+public partial class Alumno
+{
+    public int Id { get; set; }
+
+    public string Nombres { get; set; } = null!;
+
+    public string Apellidos { get; set; } = null!;
+
+    public string Genero { get; set; } = null!;
+
+    public DateTime FechaRegistro { get; set; }
+}
